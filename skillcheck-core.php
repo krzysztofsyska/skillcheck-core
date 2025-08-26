617a139a10d4b27bd519cd8d2bad2085059225be
krzysztofsyska <krzysztof.syskaa@gmail.com>
2025-08-26 22:27:04 +0200

    Update skillcheck-core.php

--- skillcheck-core.php
+++ skillcheck-core.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * Plugin Name: SkillCheck Core
+ * Description: Rdzeń funkcji SkillCheck (testy, PDF, integracje).
+ * Version: 0.1.0
+ * Author: SkillCheck
+ * Text Domain: skillcheck
+ */
 
+if (!defined('ABSPATH')) exit;
+
+define('SKC_VERSION', '0.1.0');
+define('SKC_PATH', plugin_dir_path(__FILE__));
+define('SKC_URL', plugin_dir_url(__FILE__));
+
+// Tymczasowo puste – potem dołączymy resztę plików
